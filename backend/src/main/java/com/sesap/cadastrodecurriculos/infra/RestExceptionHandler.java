f52c3a51693fc9642629089ccc3f55b783c2d22f
Matheus Dantas <129445416+theussilvas@users.noreply.github.com>
2025-07-07 18:51:56 -0300

    refactor: remove code smell do package entity

--- backend/src/main/java/com/sesap/cadastrodecurriculos/infra/RestExceptionHandler.java
+++ backend/src/main/java/com/sesap/cadastrodecurriculos/infra/RestExceptionHandler.java
@@ -11,55 +11,55 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TipoArquivoErradoException.class)
-    private ResponseEntity<RestErrorMessage> tipoArquivoErradoHandler(TipoArquivoErradoException exception){
+    protected ResponseEntity<RestErrorMessage> tipoArquivoErradoHandler(TipoArquivoErradoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE,exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(mensagemTratada);
     }
 
     @ExceptionHandler(CargoNaoEncontradoException.class)
-    private ResponseEntity<RestErrorMessage> cargoNaoEncontradoHandler(CargoNaoEncontradoException exception){
+    protected ResponseEntity<RestErrorMessage> cargoNaoEncontradoHandler(CargoNaoEncontradoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(mensagemTratada);
     }
 
     @ExceptionHandler(EmailNaoEncontradoException.class)
-    private ResponseEntity<RestErrorMessage> emailNaoEncontradoHandler(EmailNaoEncontradoException exception){
+    protected ResponseEntity<RestErrorMessage> emailNaoEncontradoHandler(EmailNaoEncontradoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(mensagemTratada);
     }
 
     @ExceptionHandler(EscolaridadeNaoEncontradaException.class)
-    private ResponseEntity<RestErrorMessage> escolaridadeNaoEncontradoHandler(EscolaridadeNaoEncontradaException exception){
+    protected ResponseEntity<RestErrorMessage> escolaridadeNaoEncontradoHandler(EscolaridadeNaoEncontradaException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(mensagemTratada);
     }
 
     @ExceptionHandler(NomeNaoEncontradoException.class)
-    private ResponseEntity<RestErrorMessage> nomeNaoEncontradoHandler(NomeNaoEncontradoException exception){
+    protected ResponseEntity<RestErrorMessage> nomeNaoEncontradoHandler(NomeNaoEncontradoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(mensagemTratada);
     }
 
     @ExceptionHandler(TamanhoInvalidoArquivoException.class)
-    private ResponseEntity<RestErrorMessage> tamanhoInvalidoArquivoHanlder(TamanhoInvalidoArquivoException exception){
+    protected ResponseEntity<RestErrorMessage> tamanhoInvalidoArquivoHanlder(TamanhoInvalidoArquivoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.PAYLOAD_TOO_LARGE, exception.getMessage());
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(mensagemTratada);
     }
 
     @ExceptionHandler(TelefoneNaoEncontradoException.class)
-    private ResponseEntity<RestErrorMessage> telefoneNaoEncontradoHandler(TelefoneNaoEncontradoException exception){
+    protected ResponseEntity<RestErrorMessage> telefoneNaoEncontradoHandler(TelefoneNaoEncontradoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(mensagemTratada);
     }
 
     @ExceptionHandler(NumeroInvalidoException.class)
-    private ResponseEntity<RestErrorMessage> numeroInvalidoHandler(NumeroInvalidoException exception){
+    protected ResponseEntity<RestErrorMessage> numeroInvalidoHandler(NumeroInvalidoException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.LENGTH_REQUIRED, exception.getMessage());
         return ResponseEntity.status(HttpStatus.LENGTH_REQUIRED).body(mensagemTratada);
     }
 
     @ExceptionHandler(NumeroRegiaoInvalidaException.class)
-    private ResponseEntity<RestErrorMessage> numeroRegiaoInvalidaHandler(NumeroRegiaoInvalidaException exception){
+    protected ResponseEntity<RestErrorMessage> numeroRegiaoInvalidaHandler(NumeroRegiaoInvalidaException exception){
         RestErrorMessage mensagemTratada = new RestErrorMessage(HttpStatus.NOT_ACCEPTABLE, exception.getMessage());
         return  ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(mensagemTratada);
     }
